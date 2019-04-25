9f7f78feacd066ee3dcce1f930a25672e20808be
eSecRPM <esecrpm@users.noreply.github.com>
2019-04-24 23:18:37 -0400

    Update install_sift.sh

--- install_sift.sh
+++ install_sift.sh
@@ -2,7 +2,7 @@
 wget -P ~/Downloads https://github.com/sans-dfir/sift-cli/releases/download/v1.7.1/sift-cli-linux
 wget -P ~/Downloads https://github.com/sans-dfir/sift-cli/releases/download/v1.7.1/sift-cli-linux.sha256.asc
 gpg --keyserver hkp://pgp.mit.edu:80 --recv-keys 22598A94
-gpg --verify sift-cli-linux.sha256.asc
-shasum -a 256 -c sift-cli-linux.sha256.asc
-sudo mv sift-cli-linux /usr/local/bin/sift
+gpg --verify ~Downloads/sift-cli-linux.sha256.asc
+shasum -a 256 -c ~Downloads/sift-cli-linux.sha256.asc
+sudo mv ~/Downloads/sift-cli-linux /usr/local/bin/sift
 sudo chmod 755 /usr/local/bin/sift
